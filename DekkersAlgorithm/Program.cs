6eca0a7608c4cdd2b7af02d4f113f8163dcd667e
Wes <mortume101@gmail.com>
2020-09-24 01:53:29 -0400

    Add project files.

---
+++ DekkersAlgorithm/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DekkersAlgorithm
+{
+  class Program
+  {
+    public static int Data = 0;
+    public static bool[] RequestSection = new bool[] { false, false };
+    public static int TaskId = 0;
+
+    static void Main(string[] args)
+    {
+      Random rand = new Random();
+      ThreadPool.SetMinThreads(2, 2);
+
+      var task1 = Task.Run(async () =>
+      {
+        RequestSection[0] = true;
+        while (RequestSection[1])
+        {
+          if (TaskId != 0)
+          {
+            RequestSection[0] = false;
+            while (TaskId != 0)
+            {
+              await Task.Delay(25);
+            }
+            RequestSection[0] = true;
+          }
+        }
+        int d = Data;
+        await Task.Delay(rand.Next(100, 201));
+        d++;
+        Data = d;
+        TaskId = 1;
+        RequestSection[0] = false;
+        Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId}: Data {d}");
+      });
+
+      var task2 = Task.Run(async () =>
+      {
+        RequestSection[1] = true;
+        while (RequestSection[0])
+        {
+          if (TaskId != 1)
+          {
+            RequestSection[1] = false;
+            while (TaskId != 1)
+            {
+              await Task.Delay(25);
+            }
+            RequestSection[1] = true;
+          }
+        }
+        await Task.Delay(rand.Next(100, 201));
+        int d = Data;
+        d++;
+        Data = d;
+        TaskId = 0;
+        RequestSection[1] = false;
+        Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId}: Data {d}");
+      });
+
+      Task.WaitAll(task1, task2);
+    }
+  }
+}
---
+++ DekkersAlgorithm/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DekkersAlgorithm")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DekkersAlgorithm")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7fa5d51c-7e71-4aed-80c7-fb9d09b2fd60")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
